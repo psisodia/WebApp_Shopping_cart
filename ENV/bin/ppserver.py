068539dc57ee423e78104339cff3d4919e2318d6
Hackbright Student <info@hackbrightacademy.com>
2014-06-23 18:09:32 -0700

    adding web app

---
+++ ENV/bin/ppserver.py
@@ -0,0 +1,5 @@
+#!/home/user/ubermelon_webapp-master/ENV/bin/python
+# EASY-INSTALL-SCRIPT: 'pp==1.6.4','ppserver.py'
+__requires__ = 'pp==1.6.4'
+import pkg_resources
+pkg_resources.run_script('pp==1.6.4', 'ppserver.py')
